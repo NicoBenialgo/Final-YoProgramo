a9952dd27d85bdd69f8702dbc5d055212d3b07ba
Nicolas Federico Benialgo <nicolasbenialgo@hotmail.com>
2022-11-29 15:25:25 -0300

    Assets, diseño y fusión
    Fusión de componentes 'login', 'logo-ap' y 'social' para añadirlos en el 'header'. Se actualizaron diseños de componentes, y se añadieron algunas imágenes que formarán parte del front-end.

--- Final-YoProgramo/src/app/app-routing.module.ts
+++ Final-YoProgramo/src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RoundProgressModule} from 'angular-svg-round-progressbar';
 
 const routes: Routes = [
-  {path:'bar',component: RoundProgressModule},
-  {path:'**', pathMatch:'full', redirectTo:'bar'}
 ];
 
 @NgModule({
--- Final-YoProgramo/src/app/app.module.ts
+++ Final-YoProgramo/src/app/app.module.ts
@@ -4,14 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AboutComponent } from './components/about/about.component';
-import { LoginComponent } from './components/login/login.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { EducationComponent } from './components/education/education.component';
-import { RoundProgressModule, ROUND_PROGRESS_DEFAULTS } from 'angular-svg-round-progressbar';
 import { SkillsComponent } from './components/skills/skills.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -20,11 +16,8 @@ import { FooterComponent } from './components/footer/footer.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent,
-    SocialComponent,
     BannerComponent,
     AboutComponent,
-    LoginComponent,
     ExperienceComponent,
     EducationComponent,
     SkillsComponent,
@@ -34,15 +27,8 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RoundProgressModule
   ],
-  providers: [{
-    provide: ROUND_PROGRESS_DEFAULTS,
-    useValue: {
-      color: '#f00',
-      background: '#0f0'
-    }
-  }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
